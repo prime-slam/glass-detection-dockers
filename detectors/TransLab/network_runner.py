df5bc812084f9f3107ba34083bf8058b570c567f
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-03-31 09:12:19 +0300

    fully moved running logic to NetworkRunner

--- detectors/TransLab/network_runner.py
+++ detectors/TransLab/network_runner.py
@@ -1,13 +1,42 @@
+from __future__ import print_function
 import os
 import sys
 
 from pathlib import Path
 
 from common.network_runner_base import NetworkRunnerBase
-from tools.test_demo import Evaluator
 from segmentron.utils import options
 from segmentron.utils.default_setup import default_setup
 from segmentron.config import cfg
+
+
+import os
+import sys
+
+cur_path = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(cur_path)[0]
+sys.path.append(root_path)
+
+import logging
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from tabulate import tabulate
+from torchvision import transforms
+from segmentron.data.dataloader import get_segmentation_dataset
+from segmentron.models.model_zoo import get_segmentation_model
+from segmentron.utils.distributed import synchronize, make_data_sampler, make_batch_data_sampler
+from segmentron.config import cfg
+from segmentron.utils.options import parse_args
+from segmentron.utils.default_setup import default_setup
+from IPython import embed
+from collections import OrderedDict
+from segmentron.utils.filesystem import makedirs
+import cv2
+import numpy as np
 
 
 class NetworkRunner(NetworkRunnerBase):
@@ -35,10 +64,75 @@ class NetworkRunner(NetworkRunnerBase):
         sys.argv = [__file__] + segmentron_args
         args = options.parse_args()
         default_setup(args)
-        self.evaluator = Evaluator(args, self)
+
+        self.args = args
+        self.device = torch.device(args.device)
+
+        # image transform
+        input_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(cfg.DATASET.MEAN, cfg.DATASET.STD),
+        ])
+
+        # dataset and dataloader
+        val_dataset = get_segmentation_dataset(cfg.DATASET.NAME,
+                                               root=cfg.DEMO_DIR,
+                                               split='val',
+                                               mode='val',
+                                               transform=input_transform,
+                                               base_size=cfg.TRAIN.BASE_SIZE)
+
+        val_sampler = make_data_sampler(val_dataset, shuffle=False, distributed=args.distributed)
+        val_batch_sampler = make_batch_data_sampler(val_sampler, images_per_batch=cfg.TEST.BATCH_SIZE, drop_last=False)
+
+        self.val_loader = data.DataLoader(dataset=val_dataset,
+                                          batch_sampler=val_batch_sampler,
+                                          num_workers=cfg.DATASET.WORKERS,
+                                          pin_memory=True)
+        self.classes = val_dataset.classes
+        # create network
+        self.model = get_segmentation_model(self.device).to(self.device)
+
+        if hasattr(self.model, 'encoder') and cfg.MODEL.BN_EPS_FOR_ENCODER:
+            logging.info('set bn custom eps for bn in encoder: {}'.format(cfg.MODEL.BN_EPS_FOR_ENCODER))
+            self.set_batch_norm_attr(self.model.encoder.named_modules(), 'eps', cfg.MODEL.BN_EPS_FOR_ENCODER)
+
+        if args.distributed:
+            self.model = nn.parallel.DistributedDataParallel(self.model,
+                                                             device_ids=[args.local_rank],
+                                                             output_device=args.local_rank, find_unused_parameters=True)
+
+        self.model.to(self.device)
+        self.count_easy = 0
+        self.count_hard = 0
+
+    def set_batch_norm_attr(self, named_modules, attr, value):
+        for m in named_modules:
+            if isinstance(m[1], nn.BatchNorm2d) or isinstance(m[1], nn.SyncBatchNorm):
+                setattr(m[1], attr, value)
 
     def run(self):
-        self.evaluator.eval()
+        self.model.eval()
+        if self.args.distributed:
+            model = self.model.module
+        else:
+            model = self.model
+
+        for (image, _, filename) in tqdm(self.val_loader):
+            image = image.to(self.device)
+            filename = filename[0]
+            save_name = os.path.basename(filename).replace('.jpg', '').replace('.png', '')
+
+            ori_img = cv2.imread(filename)
+
+            with torch.no_grad():
+                output, output_boundary = model.evaluate(image)
+                h, w, _ = ori_img.shape
+
+                glass_res = output.argmax(1)[0].data.cpu().numpy().astype('uint8') * 127
+                glass_res = cv2.resize(glass_res, (w, h), interpolation=cv2.INTER_NEAREST)
+
+            self._write_img(save_name, glass_res)
 
     def _predict(self, img, meta):
         pass
@@ -47,7 +141,8 @@ class NetworkRunner(NetworkRunnerBase):
         pass
 
     def _write_img(self, img_name, prediction):
-        pass
+        save_path = self.output_dir
+        cv2.imwrite(os.path.join(save_path, '{}_glass.png'.format(img_name)), prediction)
 
     def _load_model(self, model_path):
         pass
